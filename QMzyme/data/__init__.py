ae1fe7bf1b8e5d027d9db5b04294edda7f0f5261
Young Woo Kim <kyw3000400@gmail.com>
2026-04-29 00:33:10 -0500

    Adding more data files

--- QMzyme/data/__init__.py
+++ QMzyme/data/__init__.py
@@ -9,6 +9,10 @@ TOP = (_data_ref / '1oh0_equ.prmtop').as_posix()
 RST = (_data_ref / '1oh0_equ.rst7').as_posix()
 DCD = (_data_ref / '1oh0_equ.prod_1.stripped.dcd').as_posix()
 PQR = (_data_ref / '1oh0_equ.prod_1.stripped.pqr').as_posix()
+CSA_holo = (_data_ref / 'CSA_holo.log').as_posix()
+CSA_apo = (_data_ref / 'CSA_apo.log').as_posix()
+CSA_pkl = (_data_ref / 'CSA_holo_apo.pkl').as_posix()
+Cutoff_3 = (_data_ref / '1OH0_cutoff3.pdb').as_posix()
 
 # protein_residues = QMzyme.configuration.protein_residues
 # residue_charges = QMzyme.configuration.residue_charges
